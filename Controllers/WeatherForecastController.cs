6c3f6517ea7fb0ab09d7e0e64538afe8dc89ed29
magrni <nigrom@hotmail.com>
2024-07-09 20:49:52 -0300

    .

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -12,10 +12,12 @@ namespace TestRepo.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _config;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
@@ -30,16 +32,11 @@ namespace TestRepo.Controllers
             .ToArray();
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("data")]
+        public IActionResult GetData()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var conn = _config["Db:ConnectionString"];
+            return Ok(conn);
         }
     }
 }
